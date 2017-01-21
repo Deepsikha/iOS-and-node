0dc6899ec8bc632aaa409288d07f3e4e0c7abeff
Deepsikha <lanetteam.milans@gmiail.com>
2017-01-21 17:39:01 +0530

    mongodb schema validation

--- mongodb/demo1.js
+++ mongodb/demo1.js
@@ -17,19 +17,19 @@ app.use(cookieParser());
 
 module.exports = app;
 
-router.route('/studs')
+//router.route('/studs')
 // create a stud (accessed at POST http://localhost:8080/api/studs)
 
-    .get(function (req, res) {
+    app.get('/',function (req, res) {
         Stud.find(function (err, data) {
             if (err)
                 res.send(err);
 
             res.json(data);
-        });
+        }).select('-marks');
     })
 
-    .post(function (req, res) {
+    app.post('/create',function (req, res) {
         var stud = new Stud();      // create a new instance of the stud model
 
         stud.name = req.body.name;  // set the bears name (comes from the request)
@@ -42,10 +42,11 @@ router.route('/studs')
         });
     })
 
-router.route('/studs/:sid')
+//router.route('/studs/:sid')
 
 // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
-    .get(function (req, res) {
+    app.get('/studs/:sid',function (req, res) {
+        Stud.validate();
         Stud.findById(req.params.sid, function (err, data) {
             if (err)
                 res.send(err);
@@ -53,7 +54,7 @@ router.route('/studs/:sid')
         });
     })
 
-    .put(function (req, res) {
+    app.put('/studs/:sid',function (req, res) {
 
         // use our bear model to find the bear we want
         Stud.findById(req.params.sid, function (err, data) {
@@ -76,7 +77,7 @@ router.route('/studs/:sid')
         });
     })
 
-    .delete(function (req, res) {
+    app.delete('/studs/:sid',function (req, res) {
         Stud.remove({
             _id: req.params.sid
         }, function (err, stud) {
@@ -88,7 +89,7 @@ router.route('/studs/:sid')
     });
 
 // all of our routes will be prefixed with /api
-app.use('/api', router);   //for eg /api/studs
+//app.use('/api', router);   //for eg /api/studs
 
 var server = app.listen(8085, function () {
 
